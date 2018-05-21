7d4d3e894f1de0490d8e575cf846d0d81e051e40
LQ-Erio <37233819+TouwaErioH@users.noreply.github.com>
2018-05-21 21:41:15 +0800

    KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -4,26 +4,26 @@
 
 void cal_next(char *str, int *next, int len)
 {
-    next[0] = -1;//next[0]初始化为-1，-1表示不存在相同的最大前缀和最大后缀
-    int k = -1;//k初始化为-1
+    next[0] = -1;//next[0]初始化为-1
+    int k = -1;
     for (int q = 1; q <= len-1; q++)
     {
-        while (k > -1 && str[k + 1] != str[q])//如果下一个不同，那么k就变成next[k]，注意next[k]是小于k的，无论k取任何值。
+        while (k > -1 && str[k + 1] != str[q])
         {
             k = next[k];//往前回溯
         }
-        if (str[k + 1] == str[q])//如果相同，k++
+        if (str[k + 1] == str[q])
         {
             k = k + 1;
         }
-        next[q] = k;//这个是把算的k的值（就是相同的最大前缀和最大后缀长）赋给next[q]
+        next[q] = k;
     }
 }
 
     int KMP(char *str, int slen, char *ptr, int plen)
    {
     int *next = new int[plen];
-    cal_next(ptr, next, plen);//计算next数组
+    cal_next(ptr, next, plen);
     int k = -1;
     for (int i = 0; i < slen; i++)
     {
